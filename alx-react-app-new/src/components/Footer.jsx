36d8916bc60edf625d5383d0045095a1b5c8a888
AGUENAOU-AYOUB <aguenaouayoub04@gmail.com>
2025-07-19 04:00:22 +0100

    commit

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer style={{minHeight:'150px', display:'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: 'orange'}}>
+        <p style={{color: '#011c40', fontWeight: '500', fontSize: '16px', fontStyle: 'italic'}}>© 2023 City Lovers</p>
+      </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+      <main style={{minHeight:'200px', display:'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: 'red'}}>
+        <p style={{color: '#011c40', fontWeight: '500', fontSize: '18px', fontStyle: 'italic'}}>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    );
+}
+
+export default MainContent;
