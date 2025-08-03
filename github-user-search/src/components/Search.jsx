8c180cb66c0271e40fc0396dee475df62c1db56f
AGUENAOU-AYOUB <aguenaouayoub04@gmail.com>
2025-08-03 13:12:52 +0100

    inhance searching

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,55 +1,148 @@
 import fetchUserData from "../services/githubService";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
+
+const ITEMS_PER_PAGE = 6;
 function SearchBar() {
   const [username, setUsername] = useState("");
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [userData, setUserData] = useState(null);
-
-  const handleChange = (e) => {
+  const [error, setError] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [location, setLocation] = useState("");
+  const [repo, setRepo] = useState("");
+  const [totalCount, setTotalCount] = useState(0);
+  const [page, setPage] = useState(1);
+  const hasMoreResult = users.length < totalCount;
+  const handleChangeUsername = (e) => {
     setUsername(e.target.value);
+  };
+  const handleChangeLocation = (e) => {
+    setLocation(e.target.value);
+  };
+  const handleChangeRepo = (e) => {
+    setRepo(e.target.value);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setLoading(true);
-    setError(null);
-    setUserData(null);
+    setError(false);
+    setUsers([]);
+    setPage(1);
+    setTotalCount(0);
     try {
-      const data = await fetchUserData(username);
-      setUserData(data);
+      const data = await fetchUserData(
+        username,
+        location,
+        repo,
+        1,
+        ITEMS_PER_PAGE
+      );
+      setUsers(data.items);
+      setTotalCount(data.total_count);
     } catch (error) {
       setError(true);
     } finally {
       setLoading(false);
     }
   };
+  const handleLoadMore = async () => {
+    setLoading(true);
 
+    try {
+      const nextPage = page + 1;
+      const { items } = await fetchUserData(
+        username,
+        location,
+        repo,
+        nextPage,
+        ITEMS_PER_PAGE
+      );
+      setUsers((prevUsers) => [...prevUsers, ...items]);
+      setPage(nextPage);
+    } catch (error) {
+      console.error("no more");
+    } finally {
+      setLoading(false);
+    }
+  };
+  const showUsers = users && users.length > 0;
   return (
-    <>
+    <div className="container">
       <form className="searchBar" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Search..."
-          className="searchInput"
-          onChange={handleChange}
-        />
-        <button type="submit" >
+        <div className="titles">
+          <h1>GITHUB</h1>
+          <h2>USER SEARCH</h2>
+        </div>
+        <div className="inputs">
+          <input
+            type="text"
+            placeholder="Username"
+            className="searchInput"
+            onChange={handleChangeUsername}
+          />
+          <input
+            type="text"
+            placeholder="Location"
+            className="searchInput"
+            onChange={handleChangeLocation}
+          />
+          <input
+            type="text"
+            placeholder="MinRepo"
+            className="searchInput"
+            onChange={handleChangeRepo}
+          />
+        </div>
+        <button type="submit" className="button">
           Search
         </button>
       </form>
       <div className="result">
-        {loading && <p>Loading...</p>}
+        {loading && users.length === 0 && <p>Loading...</p>}
         {error && <p>Looks like we cant find the user</p>}
-        {userData && (
-          <div>
-            <h1>{userData.login}</h1>
-            <img src={userData.avatar_url} />
-            <a href={userData.html_url}>Link</a>
-          </div>
+        {showUsers && (
+  <ul className="userList">
+    {users.map((user) => (
+      <li key={user.id} className="list">
+        <img
+          src={user.avatar_url}
+          className="picture"
+          alt={`${user.login}'s avatar`}
+        />
+        <div className="userInfo">
+        <h3>{user.login}</h3>
+        {user.location && <p>Location: {user.location}</p>}
+
+       
+        {user.public_repos && <p>Repositories: {user.public_repos}</p>}
+        </div>
+        <a
+          href={user.html_url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="discoverLink"
+        >
+          Discover
+        </a>
+      </li>
+    ))}
+  </ul>
+)}
+        {!loading && users.length === 0 && !error && (
+          <p>No users found with the provided criteria.</p>
+        )}
+        {hasMoreResult && (
+          <button
+            className="loadMore"
+            onClick={handleLoadMore}
+            disabled={loading}
+          >
+            {loading ? "Loading..." : "Load More..."}
+          </button>
         )}
       </div>
-    </>
+    </div>
   );
 }
 export default SearchBar;
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,15 +2,44 @@ import axios from "axios";
 
 const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
-async function fetchUserData(username) {
-    const response = await axios.get(`https://api.github.com/users/${username}`, {
+async function fetchUserData(username, location, repo, page = 1, per_page = 5) {
+  const queryArray = [];
+  if (username) {
+    queryArray.push(username);
+  }
+  if (location) {
+    queryArray.push(`location:${location}`);
+  }
+  if (repo) {
+    queryArray.push(`repos:>=${repo}`);
+  }
+  if (queryArray.length === 0) {
+    return { items: [], total_cout: 0 };
+  }
+  const queryString = queryArray.join("+");
+  const searchResponse = await axios.get(
+    `https://api.github.com/search/users?q=${queryString}&page=${page}&per_page=${per_page}`,
+    {
       headers: {
         Authorization: `token ${apiKey}`,
       },
+    }
+  );
+  const users = searchResponse.data.items;
+  const detailedUsersPromises = users.map(async (user) => {
+    const detailedResponse = await axios.get(user.url, {
+      headers: {
+        Authorization: `tokaen ${apiKey}`,
+      },
     });
-
-    return response.data;
- 
+    return{...user, ...detailedResponse.data};
+  });
+  const detailedUsers = await Promise.all(detailedUsersPromises)
+  return {
+    items: detailedUsers,
+    total_count: searchResponse.data.total_count,
+  };
+  
 }
 
 export default fetchUserData;
