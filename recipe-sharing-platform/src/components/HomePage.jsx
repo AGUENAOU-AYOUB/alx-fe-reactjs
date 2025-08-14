bf64c23b6bb33c0178aef20e0fa183f842ff7be5
AGUENAOU-AYOUB <aguenaouayoub04@gmail.com>
2025-08-14 03:44:24 +0100

    building the homepage

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import FoodCard from "./foodCard";
+function HomePage() {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("./data.json");
+        const data = await response.json();
+        setRecipes(data);
+      } catch {
+        console.log("no recipes found");
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchRecipes();
+  }, []);
+
+  return (
+    <div className="w-full h-full bg-white grid grid-cols-1 grid-rows-[1fr_5fr]  ">
+      <div className="row-start-1 row-end-2 flex flex-col py-[10%] gap-[2vh] md:h-[20vh] md:py-[1vh] md:mb-[2vh] bg-[#1b4332] ">
+        <div className=" w-[50%] h-[50%] font-poppins  text-2xl text-center m-auto md:h-[5vh]">
+          Hello There!
+        </div>
+        <input
+          className="w-[70%] h-12 bg-white m-auto rounded-[22px] px-4 shadow-[0px_0px_20px_0px_rgba(0,0,0,0.25)] font-regular  text-gray-500 md:h-[5vh] "
+          placeholder="Search For Reciepes"
+        />
+      </div>
+      <div className="w-full h-full bg-white row-start-2 row-end-3  text-black flex sm:flex-col overflow-y-scroll overflow-x-hidden px-[5%] md:px-[10px] md:flex-row gap-[5vh] md:gap-[20px] flex-wrap py-[2vh] md:py-[5vh]">
+        {loading && <p>loading...</p>}
+        {error && <p>error</p>}
+        {recipes.map((recipe) => (
+          <FoodCard key={recipe.id} {...recipe}/>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default HomePage;
--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {fontFamily: {
+        // Use `font-poppins` class
+        poppins: ["Poppins", "ui-sans-serif", "system-ui", "-apple-system", "Segoe UI", "Roboto", "Helvetica Neue", "Arial"],
+        // Make Poppins the default sans (optional)
+        sans: ["Poppins", "ui-sans-serif", "system-ui", "-apple-system", "Segoe UI", "Roboto", "Helvetica Neue", "Arial"],
+      },},
   },
   plugins: [],
 }
