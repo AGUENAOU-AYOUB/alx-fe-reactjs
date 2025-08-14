09fe0f3bf8f443d74c6b78fbc0f7c065ddc7384f
AGUENAOU-AYOUB <aguenaouayoub04@gmail.com>
2025-08-14 04:01:14 +0100

    changed the plcaement o data.json

--- recipe-sharing-platform/src/components/FoodCard.jsx
+++ recipe-sharing-platform/src/components/FoodCard.jsx
@@ -1,6 +1,6 @@
 function FoodCard({ image, title, summary }) {
   return (
-    <article className="group w-[90%] sm:w-[220px] md:w-[280px] lg:w-[320px] bg-[#1B4332] rounded-2xl overflow-hidden shadow transition-all duration-500 ease-in-out transform-gpu hover:-translate-y-4 hover:shadow-lg mx-auto ">
+    <div className="group w-[90%] sm:w-[220px] md:w-[280px] lg:w-[320px] bg-[#1B4332] rounded-2xl overflow-hidden shadow transition-all duration-500 ease-in-out transform-gpu hover:-translate-y-4 hover:shadow-lg mx-auto ">
       <div className="relative aspect-[4/3] overflow-hidden">
         <img
           src={image}
@@ -10,7 +10,7 @@ function FoodCard({ image, title, summary }) {
       </div>
       <h3 className="p-3 text-white text-lg font-poppins ">{title}</h3>
       <p className="p-3 text-white text-[10px] font-poppins">{summary}</p>
-    </article>
+    </div>
   );
 }
 export default FoodCard;
--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -7,7 +7,7 @@ function HomePage() {
   const [error, setError] = useState(false);
   useEffect(() => {
      setRecipes(data);
-  }, []);
+  }, [data]);
 
   return (
     <div className="w-full h-full bg-white grid grid-cols-1 grid-rows-[1fr_5fr]  ">
@@ -20,11 +20,21 @@ function HomePage() {
           placeholder="Search For Reciepes"
         />
       </div>
-      <div className="w-full h-full bg-white row-start-2 row-end-3  text-black flex sm:flex-col overflow-y-scroll overflow-x-hidden px-[5%] md:px-[10px] md:flex-row gap-[5vh] md:gap-[20px] flex-wrap py-[2vh] md:py-[5vh]">
+      <div className="w-full h-full bg-white row-start-2 row-end-3  text-black   overflow-y-scroll overflow-x-hidden px-[5%] md:px-[10px]  gap-[5vh]  py-[2vh] md:py-[5vh] grid grid-cols-2 md:grid-cols-3 ">
         {loading && <p>loading...</p>}
         {error && <p>error</p>}
         {recipes.map((recipe) => (
-          <FoodCard key={recipe.id} {...recipe}/>
+         <div className="group w-[90%] sm:w-[220px] md:w-[280px] lg:w-[320px] bg-[#1B4332] rounded-2xl overflow-hidden shadow transition-all duration-500 ease-in-out transform-gpu hover:-translate-y-4 hover:shadow-lg mx-auto ">
+      <div className="relative aspect-[4/3] overflow-hidden">
+        <img
+          src={recipe.image}
+          alt={recipe.title}
+          className="w-full h-full object-cover object-center transition-transform duration-500 ease-in-out transform-gpu group-hover:scale-110"
+        />
+      </div>
+      <h3 className="p-3 text-white text-lg font-poppins ">{recipe.title}</h3>
+      <p className="p-3 text-white text-[10px] font-poppins">{recipe.summary}</p>
+    </div>
         ))}
       </div>
     </div>
